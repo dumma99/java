aa2193aad55eacd4ed10bac143f921e3d6667741
dumma99 <dumma3214@gmail.com>
2025-02-10 02:35:23 -0800

    Add files via upload

---
+++ Energetic.java
@@ -0,0 +1,5 @@
+public class Energetic{
+	public static void main(String args[]){
+		System.out.println("session completed, pending session is practical");
+	}
+}
---
+++ Room.java
@@ -0,0 +1,6 @@
+public class Room{
+ public static void main(String args[]){
+ System.out.println("Room");
+ }
+ }
+ 
